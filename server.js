88f506fce2c5a4bca901d3ce89c8eef63fc7754b
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-11 15:16:09 +0100

    Advanced Templating techniques, Partials and Middleware sections added.

--- server.js
+++ server.js
@@ -1,23 +1,52 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
-// app.use - express static to export assets
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server log.');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next)=>{
+//     res.render('maintainence.hbs',{
+//         pageTitle: 'Maintainence Page'
+//     });
+// });
+
+// app.use to register middleware
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express!<h1>');
-    res.send({
-        name: 'Arun',
-        likes: [
-            'photography',
-            'cities'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to the website!'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About page'
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -26,6 +55,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Server running at port 3000');
 });
